7785382f91bfe637ca935d63555ba4c1255cfbad
SirLich <lrkoehler77@gmail.com>
2023-01-08 20:16:48 +0100

    Final Polish (pre export)

--- game/preview_world.gd
+++ game/preview_world.gd
@@ -6,6 +6,9 @@ func _ready():
 	Gamestate.generate_mushrooms()
 	Gamestate.place_mushrooms(false)
 	%LevelsToPlay.text = str(Gamestate.level) + "/" + str(Gamestate.total_levels)
+	
+	if Gamestate.level != 0:
+		Gamestate.play_sound_win()
 
 func _on_button_pressed():
 	get_tree().change_scene_to_packed(playScene)
